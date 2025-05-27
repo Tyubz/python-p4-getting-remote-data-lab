215292d1f0e15c94bdafae6def7cfd069a18f3dd
victor <vktubei@gmail.com>
2025-05-27 17:33:06 +0300

    Completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,17 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+     response = requests.get(self.url)
+     response.raise_for_status()
+     print("Response Text:", response.text) 
+     return response.content.decode('utf-8')
+
 
     def load_json(self):
-        pass
+        """Uses get_response_body to fetch data and converts it to a Python list or dictionary."""
+        response_body = self.get_response_body()
+        return json.loads(response_body)
